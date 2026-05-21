bec8e45e85b603ade08e16386a48fb755280d8d1
Felipe Neyra <fenerey06@gmail.com>
2026-05-20 20:32:51 -0400

    Correción de errores de disposición de me
    Se corrigen errores en el posicionamiento de botones y del propio menú hamburguesa al activar a este último.

--- assets/JS/menu_hamburguesa.js
+++ assets/JS/menu_hamburguesa.js
@@ -1,19 +1,16 @@
 //Menú Hamburguesa
 window.onload = () => {
+    //Obtener una referencia del botón y del menú propio
     const botonMenu =
     document.getElementById("btn-hamburguer");
 
     const menu =
         document.getElementById("menu-navegacion");
-        
-    function toggleMenu() {
+    
+    //Aplicar una función para poder hacer click en el botón
+    botonMenu.addEventListener("click",()=>{
+        //Activamos el dezplamiento y muestra del menú en CSS
         menu.classList.toggle("active");
         botonMenu.classList.toggle("active");
-
-    }
-
-    botonMenu.addEventListener(
-        "click",
-        toggleMenu
-    );
+    })
 }
